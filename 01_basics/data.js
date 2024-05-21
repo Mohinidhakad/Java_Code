741f701cb573771e28ebe8bc07511df8b919ee4b
Mohini Dhakad <99878435+Mohinidhakad@users.noreply.github.com>
2024-05-21 12:18:54 +0530

    Add files via upload
    interview basic que

---
+++ 01_basics/data.js
@@ -0,0 +1,38 @@
+//  let name ="Mohini" //string
+//  let age = 10 //no.
+//  let isloggedin = false //boolian
+ //nul is comsidered as object when you see its type
+ //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ 
+
+ /*conversions and operations*/
+
+//  let score =33
+//  console.log(typeof score);
+//  console.log(typeof(score));
+
+//  let valueInNumber = Number(score)
+//  console.log(typeof valueInNumber);// print number
+ // check the no.
+//  let score ="33adf"
+//  let valueInNumber = Number(score)
+//  console.log(valueInNumber); // not a no. 
+// let isLoggedIn =""
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+let someNumber =33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// console.log("1" + 2);//12
+// console.log(1 + 2);//3
+// console.log(1 + "2");//12
+// console.log("1" + 2 +2);//122
+// console.log(1 + 2+"2" );//32
+// console.log(3 + 2*3);
+// console.log(+true);//1
+
+
+
